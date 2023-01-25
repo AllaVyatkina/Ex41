49512c08b943b709d9b64fdc6c7f030940b2011a
Alla <bakrlona@mail.ru>
2023-01-25 23:01:54 +0500

    Ex41

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите любое количество чисел от -1000 до 1000 ЧЕРЕЗ ПРОБЕЛ: ");
+    int[] newarray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int count = 0;
+for (int i = 0; i < newarray.Length; i++)
+{
+    if (newarray[i] > 0)
+    {
+        count++;
+  }
+
+}
+    Console.WriteLine($"Введено чисел больше 0: {count}");
